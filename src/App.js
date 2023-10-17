48dc34691a7a998df0b6abddbab15b8d74d9e78f
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-10-17 19:34:16 +1100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <Search />
+
       <p>
         {" "}
-        <a href="https://github.com/Lia-O1/weather-react">Open-sourced</a> by
-        Olha Stepko
+        <a href="https://github.com/Lia-O1/weather-react" target="_blank">
+          Open-sourced
+        </a>{" "}
+        by Olha Stepko
       </p>
     </div>
   );
